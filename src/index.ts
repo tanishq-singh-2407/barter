7f08be6e50ba513ff35bb26f61a9a2a350f4d878
Tanishq Singh <tanishqsingh640@gmail.com>
2023-02-13 08:45:52 +0000

    :zap: [feat]: `scraper` ready

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,32 @@ import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 import chromium from 'chrome-aws-lambda';
 import aws from 'aws-sdk';
 import { Browser } from "puppeteer-core";
+import { createHash } from 'crypto';
 
-const url = "https://api.ipify.org";
 const isDev = process.env.dev === "true";
+
+type Body = {
+    url: string | null;
+    address: string | null;
+    token: string | null;
+}
 
 export const handler = async ({ body }: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
     let response: APIGatewayProxyResult;
     let browser: Browser;
+    let targetURL: URL;
+
+    const { url, address, token } = JSON.parse(body) as Body;
+
+    try {
+        if (!url || !address || !token) throw new Error('URL, Address, or token field missing');
+        if (!address.startsWith("nano_") || address.length !== 65) throw new Error('Nano address invalid.');
+        if (createHash('sha256').update(token).digest('hex') !== "9d8a178b8c1b8cea103082bf57616e99c82301be8f7e74717126c6f0458dbc2e") throw new Error("Token not matched");
+
+        targetURL = new URL(url);
+    } catch (error) {
+        return { statusCode: 400, body: JSON.stringify({ error: error.message }) };
+    }
 
     const startTime = Date.now();
 
@@ -40,21 +59,33 @@ export const handler = async ({ body }: APIGatewayEvent): Promise<APIGatewayProx
     const page = await browser.newPage();
 
     try {
-        await page.goto(url, { waitUntil: "domcontentloaded" });
+        await page.goto(url, { waitUntil: "networkidle2" });
+        await page.type("#address", address);
+        const req1 = await page.waitForResponse(res => res.url() === "https://api.nanswap.com/get-free" && res.request().method() === "POST", { timeout: 6000 });
+        const res1 = await req1.json();
 
-        const ip = await page.$eval('body', el => el.textContent);
-        const responseTime = Date.now() - startTime;
+        await page.reload();
+        await page.type("#address", address);
+        const req2 = await page.waitForResponse(res => res.url() === "https://api.nanswap.com/get-free" && res.request().method() === "POST", { timeout: 6000 });
+        const res2 = await req2.json();
+
+        await page.reload();
+        await page.type("#address", address);
+        const req3 = await page.waitForResponse(res => res.url() === "https://api.nanswap.com/get-free" && res.request().method() === "POST", { timeout: 6000 });
+        const res3 = await req3.json();
+        
+        const timetaken = Date.now() - startTime;
 
         response = {
             statusCode: 200,
-            body: JSON.stringify({ ip, responseTime })
+            body: JSON.stringify({ response: { res1, res2, res3 }, timetaken })
         }
     } catch (error) {
-        const responseTime = Date.now() - startTime;
+        const timetaken = Date.now() - startTime;
 
         response = {
             statusCode: 400,
-            body: JSON.stringify({ error: error.message, responseTime })
+            body: JSON.stringify({ error: error.message, timetaken })
         }
     } finally {
         await page.close();
--- src/local.ts
+++ src/local.ts
@@ -2,8 +2,17 @@ import { handler } from './index';
 
 (async () => {
     const event: any = {
-        body: "j"
+        body: JSON.stringify({
+            url: 'https://nanswap.com/nano-faucet',
+            address: "nano_1t8qn46b8h8qij59nydqgz38d37rz63nhkwe7d7ubzgx15p5igtqrnsm3gqq",
+            token: ""
+        })
     }
 
-    console.log(await handler(event));
+    const { body, statusCode } = await handler(event);
+
+    console.log({
+        statusCode,
+        body: JSON.parse(body)
+    });
 })();
